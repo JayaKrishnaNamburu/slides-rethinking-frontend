761a8aca8d52d7752bdd871d303b4e3c9c6b1a44
JayaKrishna <namburu1995@gmail.com>
2019-12-07 11:57:29 +0530

    Re-configured slides

---
+++ src/slides/cry.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./cry.module.css";
+
+const Cry = props => {
+  return (
+    <div className={styles.container}>
+      <img src="https://media.giphy.com/media/jnQYWZ0T4mkhCmkzcn/giphy.gif" />
+    </div>
+  );
+};
+
+export default Cry;
--- src/slides/index.js
+++ src/slides/index.js
@@ -1,5 +1,6 @@
 import One from "./one";
 import Two from "./two";
+import Cry from "./cry";
 import Work from "./drive-work";
 import Three from "./three";
 import Four from "./four";
@@ -10,11 +11,11 @@ import Eight from "./eight";
 import Nine from "./nine";
 import Ten from "./ten";
 import Eleven from "./eleven";
-import Twelve from "./twelve";
 import Thirteen from "./thirteen";
 import Fourteen from "./fourteen";
 import Fifteen from "./fifteen";
 import Sixteen from "./sixteen";
+import Twelve from "./twelve";
 import svelte from "./svelte";
 import Eighteen from "./eighteen";
 import Nineteen from "./nineteen";
@@ -22,6 +23,7 @@ import Nineteen from "./nineteen";
 export {
   One,
   Two,
+  Cry,
   Work,
   Three,
   Four,
