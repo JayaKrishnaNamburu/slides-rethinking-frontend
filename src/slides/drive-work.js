22d0ed162dcab6fe3215c700b29048d561884f60
JayaKrishna <namburu1995@gmail.com>
2020-01-25 10:17:02 +0530

    Slight changes

--- src/slides/drive-work.js
+++ src/slides/drive-work.js
@@ -4,7 +4,8 @@ import styles from "./drive-work.module.css";
 const Work = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.heading}>T - D - D</div>
+      {/* <div className={styles.heading}>T - D - D</div> */}
+      <div className={styles.heading}>Motto of a front-end Dev ?</div>
     </div>
   );
 };
--- src/slides/seven.js
+++ src/slides/seven.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import styles from './seven.module.css'
+import styles from "./seven.module.css";
 
-const Seven = (props) => {
+const Seven = props => {
   return (
     <div className={styles.container}>
       <span className={styles.text}>Hold on 🖐🏼</span>
-      <span className={styles.text1}>Ok, lets quickly set up stack</span>
+      <span className={styles.text1}>Ok, lets quickly set up few things..</span>
     </div>
-  )
-}
+  );
+};
 
-export default Seven
+export default Seven;
